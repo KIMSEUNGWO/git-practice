0e48d62e8a38ab160e56b18392648474d3b59f09
김승우 <tmd8633@naver.com>
2025-07-10 16:04:54 +0900

    controller 추가

---
+++ src/main/java/com/demo/git/controller/TemplateController.java
@@ -0,0 +1,13 @@
+package com.demo.git.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TemplateController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
